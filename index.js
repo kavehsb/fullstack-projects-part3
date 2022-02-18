9c3eaf285ce9913f4c05ecc92ffe63cfb39e5c01
Kaveh Buenaventura <kavehsbuen@gmail.com>
2022-02-17 16:12:24 -0800

    Complete exercise 3.3

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => id === person.id) 
+
+    if (!person) {
+        response.status(404).end()
+    } else {
+        response.json(person)
+    }
+})
+
 app.get('/info', (request, response) => {
     const date = new Date()
     response.send(
