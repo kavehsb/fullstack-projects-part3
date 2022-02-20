d3242b572e09dac6c2e1f143f6480cee7224f6ba
Kaveh Buenaventura <kavehsbuen@gmail.com>
2022-02-19 19:03:09 -0800

    Complete exercise 3.12
    Created a new .js file to practice setting up a MongoDB database.
    The functionality of this script is to allow for viewing all the
    phonebook entries in the database if the command line args only include
    the password. However, if the command line args also include a name
    and a number, the program will instantiate a new person object to be
    saved to the database.

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide password as arg: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 
+`mongodb+srv://FSOPhonebook:${password}@cluster0.dwi2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person
+    .find({})
+    .then(result => {
+        console.log('Phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    
+    person
+    .save()
+    .then(result => {
+        console.log(result)
+        console.log('person saved')
+        mongoose.connection.close()
+    })
+}
